d451988a773dca8ea698f606b474099df9167a8f
StefanoF02 <stefanof54@gmail.com>
2023-10-26 12:11:05 +0200

    Setting up the user model and connection to DB.

---
+++ src/main/java/com/example/Fooderando/User.java
@@ -0,0 +1,34 @@
+package com.example.Fooderando;
+
+public class User {
+
+    private Integer id;
+
+    private String username;
+
+    private String usermail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsermail() {
+        return usermail;
+    }
+
+    public void setUsermail(String usermail) {
+        this.usermail = usermail;
+    }
+}
