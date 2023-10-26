f8ecefb2a614932042293d82ee2df71ca5526efd
StefanoF02 <stefanof54@gmail.com>
2023-10-26 11:37:38 +0200

    First initial commit. Project created.

---
+++ src/test/java/com/example/Fooderando/FooderandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fooderando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooderandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
