e85cd5b09b65699d381cc15a6c660fe21cc98139
StefanoF02 <stefanof54@gmail.com>
2023-10-26 21:32:51 +0200

    Added PK to user`s id attribute.

--- src/main/java/com/example/EatMe/User.java
+++ src/main/java/com/example/EatMe/User.java
@@ -1,7 +1,14 @@
 package com.example.EatMe;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String username;
